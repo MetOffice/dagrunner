529f241ef1d7b5184e7db7f2e6f8928660ef4900
cpelley <carwyn.pelley@metoffice.gov.uk>
2024-06-19 00:00:22 +0100

    Missing copyright headers

--- dagrunner/runner/schedulers/base.py
+++ dagrunner/runner/schedulers/base.py
@@ -1,3 +1,7 @@
+# (C) Crown Copyright, Met Office. All rights reserved.
+#
+# This file is part of 'dagrunner' and is released under the BSD 3-Clause license.
+# See LICENSE in the root of the repository for full licensing details.
 # # Abstract base class for schedulers
 # from typing import Protocol, Any
 
