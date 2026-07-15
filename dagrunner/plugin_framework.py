8b2bd1d609cb4f4852ab59d2115fd69ac13b3801
ryan-cocking-mo <ryan.cocking@metoffice.gov.uk>
2026-07-15 14:58:28 +0100

    reword warning message

--- dagrunner/plugin_framework.py
+++ dagrunner/plugin_framework.py
@@ -163,10 +163,10 @@ class Load(Plugin):
             if self._on_missing == "error":
                 raise e
             elif self._on_missing == "ignore":
-                warnings.warn(f"Ignoring node due to error {str(e)!r}")
+                warnings.warn(f"Ignoring node due to caught exception {str(e)!r}")
                 return events.IGNORE_EVENT
             elif self._on_missing == "skip":
-                warnings.warn(f"Skipping node due to error {str(e)!r}")
+                warnings.warn(f"Skipping node due to caught exception {str(e)!r}")
                 return events.SKIP_EVENT
             else:
                 raise ValueError(
