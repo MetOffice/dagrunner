5ba6eb4fdce640282905b55b0ee40b19d7b5ed84
ryan-cocking-mo <ryan.cocking@metoffice.gov.uk>
2026-07-23 09:50:33 +0100

    revert doc changes

--- dagrunner/events.py
+++ dagrunner/events.py
@@ -7,27 +7,28 @@
 DAGrunner defines two special singleton events that plugins can return to control the
 execution flow of their graph.
 
-- IGNORE_EVENT - removes a particular input from further processing.
-- SKIP_EVENT -  aborts the execution of a plugin (and all downstream nodes) when any
+- event.IGNORE - removes a particular input from further processing.
+- event.SKIP -  aborts the execution of a plugin (and all downstream nodes) when any
   input carries this event.
 
 
-### _IgnoreEvent: `IGNORE_EVENT`
-When a plugin returns `IGNORE_EVENT`, the immediate descendant node filters out that
-input. The remaining inputs of that plugin are utilised by that node as normal.
-If all inputs to a node are `IGNORE_EVENT`, the node's execution is skipped, and
-`IGNORE_EVENT` is returned instead.
+### event.IGNORE
+When a plugin returns `event.IGNORE`, the immediate descendant node filters out that
+input.
+The remaining inputs of that plugin are utilised by that node as normal.
+If all inputs to a node are `event.IGNORE`, the node's execution is ignored, and a
+`event.IGNORE` event is returned instead, ignoring execution through all descendants.
 
 ```mermaid
 ---
-title: IGNORE_EVENT (filtering some)
+title: event.IGNORE (filtering some)
 ---
 graph
     cycle1{cycleX}
     cycle1 --> Input1 --> filepath --> Load1 --> cube1 --> Proc
-    cycle1 --> Input2 --> filepath --> Load2 --> IGNORE_EVENT --> Proc
+    cycle1 --> Input2 --> filepath --> Load2 --> event.IGNORE --> Proc
     cycle1 --> Input3 --> filepath --> Load3 --> cube3 --> Proc
-    cycle1 --> Input4 --> filepath --> Load4 --> IGNORE_EVENT --> Proc
+    cycle1 --> Input4 --> filepath --> Load4 --> event.IGNORE --> Proc
     Proc --> cube --> Save
     Proc["Proc (cube1, cube3)"]
 ```
@@ -35,24 +36,24 @@ Here, Input2 and Input4 return an IGNORE event, likely due to there being missin
 Only the non-ignored cubes (`cube1` and `cube3`) reach `Proc`; the ignored inputs are
 dropped.
 
-### _SkipEvent: `SKIP_EVENT`
-The skip event differs from the ignore event in that if **any** input to a plugin is
-`SKIP_EVENT`, node execution is skipped and `SKIP_EVENT` is returned instead. This skip
-propagates so that all dependent nodes and their descendants are not executed.
+### `event.IGNORE`
+The SKIP event differs from the IGNORE event in that if **any** input to a plugin is a
+SKIP event, node execution is skipped and it instead propagates this skip event so that
+all dependent nodes and their descendants aren't executed.
 
 ```mermaid
 ---
-title: SKIP_EVENT
+title: event.SKIP
 ---
 graph
     cycle1{cycleX}
     cycle1 --> Input1 --> filepath --> Load1 --> cube --> Proc
-    cycle1 --> Input2 --> filepath --> Load2 --> SKIP_EVENT --> Proc
+    cycle1 --> Input2 --> filepath --> Load2 --> event.SKIP --> Proc
     cycle1 --> Input3 --> filepath --> Load3 --> cube --> Proc
-    cycle1 --> Input4 --> filepath --> Load4 --> IGNORE_EVENT --> Proc
-    Proc --> SKIP_EVENT --> Save
+    cycle1 --> Input4 --> filepath --> Load4 --> event.IGNORE --> Proc
+    Proc --> event.SKIP --> Save
 ```
-Because `Input2` returns `SKIP_EVENT`, the Proc node and everything that follows aren't
+Because `Input2` returns a SKIP event, the Proc node and everything that follows aren't
 executed and neither is the Save node since the skip is propagated along the execution
 graph.
 """
